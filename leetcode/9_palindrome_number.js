4f1832ffa0d7aee8dd76ed55a1a57c8843846787
Sepand Assadi <sepand.assadi@sony.com>
2020-07-23 06:16:11 -0700

    adds palindrome number easier approach solution

--- leetcode/9_palindrome_number.js
+++ leetcode/9_palindrome_number.js
@@ -2,30 +2,55 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    if(x < 0) {
-        return false;
+var isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
+
+  var div = 1;
+
+  while (parseInt(x / div) >= 10) {
+    div *= 10;
+  }
+
+  var left, right;
+
+  while (div > 1) {
+    left = parseInt(x / div);
+    right = x % 10;
+    if (left !== right) {
+      return false;
     }
 
-    var div = 1;
+    x = x % div;
+    x = parseInt(x / 10);
+    div /= 100;
+  }
 
-    while(parseInt(x / div) >= 10) {
-        div *= 10;
-    }
+  return true;
+};
 
-    var left, right;
+// a slightly different approach (easier to read)
+const isPalindrome2 = (number) => {
+  if (number < 0) {
+    return false;
+  }
 
-    while(div > 1) {
-        left = parseInt(x / div);
-        right = x % 10;
-        if(left !== right) {
-            return false;
-        }
-
-        x = x % div;
-        x = parseInt(x/10);
-        div /= 100;
-    }
-
+  if (number < 10) {
     return true;
+  }
+
+  let reverse = 0;
+  let temp = number;
+  while (temp !== 0) {
+    const remainder = temp % 10;
+    reverse = reverse * 10 + remainder;
+    temp = Math.floor(temp / 10);
+  }
+
+  if (number === reverse) {
+    return true;
+  }
+
+  return false;
 };
